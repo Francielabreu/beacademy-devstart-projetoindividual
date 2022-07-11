a0ffc507853b572733c97eb6e31c5b820481890e
Franciel Abreu <francielabreu23@gmail.com>
2022-07-11 00:06:12 -0300

    criação da migrate de produtos

--- database/migrations/2022_07_11_024902_create_products_table.php
+++ database/migrations/2022_07_11_024902_create_products_table.php
@@ -15,6 +15,16 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('name',50);
+            $table->string('description',100);
+            $table->string('category',100);
+            $table->string('brand',100);
+            $table->string('quantity',50);
+            $table->string('provider',50);
+            $table->string('data_venc',20);
+            $table->float('price',15,6);
+            $table->float('saleprice',15,6);
             $table->timestamps();
         });
     }
