1ccc3175783d8e8d2b6964f9881cee589753d1fe
Franciel Abreu <francielabreu23@gmail.com>
2022-07-14 23:47:19 -0300

    criação da rota show

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 
 
 Route::get('/products',[ProductController::class,'index'])->name('products.index');
+Route::get('/products/{id}',[ProductController::class,'show'])->name('products.show');
