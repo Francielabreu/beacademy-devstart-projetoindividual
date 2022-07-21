4e79a36fcd0c0a4f537ad36e8da17f9ba43a1631
= <=>
2022-07-20 23:04:13 -0300

    implementação das rotas de delete

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ Route::get('/products/create',[ProductController::class,'create'])->name('produc
 Route::put('/products/{id}',[ProductController::class,'update'])->name('products.update');
 Route::get('/products/{id}/edit',[ProductController::class,'edit'])->name('products.edit');
 Route::post('/products',[ProductController::class,'store'])->name('products.store');
+Route::delete('/products/{id}',[ProductController::class,'destroy'])->name('products.destroy');
+
 Route::get('/products',[ProductController::class,'index'])->name('products.index');
 Route::get('/products/{id}',[ProductController::class,'show'])->name('products.show');
